7dc0b8f4e03486e2784e559f30c849caca44ef44
yigitsancar <ygtsncr07@gmail.com>
2026-06-01 18:04:14 +0300

    Enable CORS for React frontend

--- src/main/java/com/yigit/securebank/config/SecurityConfig.java
+++ src/main/java/com/yigit/securebank/config/SecurityConfig.java
@@ -9,6 +9,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.Customizer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -28,6 +33,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -45,4 +51,29 @@ public class SecurityConfig {
 
         return http.build();
     }
+ 
+    @Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+    	CorsConfiguration configuration = new CorsConfiguration();
+
+   	 configuration.setAllowedOrigins(List.of(
+            "http://localhost:5173",
+            "http://51.21.210.17"
+   	 ));
+
+    	configuration.setAllowedMethods(List.of(
+            "GET", "POST", "PUT", "DELETE", "OPTIONS"
+   	 ));
+
+   	 configuration.setAllowedHeaders(List.of("*"));
+   	 configuration.setAllowCredentials(true);
+
+    	UrlBasedCorsConfigurationSource source =
+            new UrlBasedCorsConfigurationSource();
+
+   	 source.registerCorsConfiguration("/**", configuration);
+
+    	return source;
+    }
+
 }
